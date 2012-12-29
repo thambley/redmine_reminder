a47e685e42675a60135daedee75b4de914bfee53
thambley <thambley@travelleaders.com>
2012-12-29 02:22:35 -0500

    bump version

--- init.rb
+++ init.rb
@@ -13,7 +13,7 @@ Redmine::Plugin.register :redmine_reminder do
   name 'Advanced reminder'
   author 'Milan Stastny of ALVILA SYSTEMS'
   description 'E-mail notification of issues due date you are involved in (Assignee, Author, Watcher)'
-  version '0.0.9'
+  version '0.0.10'
   url 'https://github.com/thambley/redmine_reminder'
   requires_redmine :version_or_higher => '2.1.0'
 end
